ef5dfea2eaa3d9e3698ef55322f1099f9e28c997
Sakana <sakanakaah@gmail.com>
2024-06-20 20:01:19 +0700

    menambahkan fungsi write

--- CRUD/Operasi.py
+++ CRUD/Operasi.py
@@ -1,3 +1,4 @@
+from time import time
 from . import  Database
 from .Util import random_string
 import time
@@ -9,8 +10,19 @@ def creat_first_data():
         tahun = input("tahun: ")
 
         data = Database.TEMPLATE.copy()
+
         data["pk"] = random_string(6)
-        data["date_time"] = time.strftime("%Y-%m-%d-%H-%M-%Sz", time.gmtime())
-        
-        print(data["date_time"])
-        tahun = input("tahun: ")
+        data["date_time"] = time.strftime("%Y-%m-%d-%H-%M-%S%z",time.gmtime())
+        data["penulis"] = penulis + Database.TEMPLATE["penulis"][len(penulis):]
+        data["judul"] = judul + Database.TEMPLATE["judul"][len(judul):]
+        data["tahun"] = tahun
+
+        data_str = f'{data["pk"]},{ data["date_time"]},{ data["penulis"]},{data["judul"]},{data["tahun"]}\n'
+        print(data_str)
+        try:
+            with open(Database.DB_NAME, "w", encoding="utf-8") as file:
+                file.write(data_str)
+        except: 
+            print("udah lah gagal bos")
+
+       
